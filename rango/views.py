5fa3b389c79bcad9e7581cad7f064275e263c9bb
Jocelyn Sickler <3112960S@student.gla.ac.uk>
2026-02-18 10:07:55 +0000

    Added visits cookie

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -14,6 +16,35 @@ from django.urls import reverse
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -32,14 +63,18 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Return a rendered response to the send to the client.
-    # We make use of the shorcut function to make out lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    
+    # Obtain our Response object early so we can add cookie information
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
 
 
 def about(request):
     context_dict = {'yourname': 'Jocelyn Sickler'}
+    context_dict['visits'] = request.session['visits']
 
     return render(request, 'rango/about.html', context=context_dict)
 
